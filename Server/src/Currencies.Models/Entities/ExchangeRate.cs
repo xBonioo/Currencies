29afd1792c5d0e7376db28c7d7fbc21401581f1d
Karina199906 <karina.sandra.tylmanowska@gmail.com>
2024-04-08 19:31:47 +0200

    update exchange rate

--- Server/src/Currencies.Models/Entities/ExchangeRate.cs
+++ Server/src/Currencies.Models/Entities/ExchangeRate.cs
@@ -8,6 +8,8 @@ public class ExchangeRate : ICreatable, IModifable
     public int FromCurrencyID { get; set; }
     public int ToCurrencyID { get; set; }
     public decimal Rate { get; set; }
+    public Direction Direction { get; set; }
+    public bool IsActive { get; set; }
     public DateTime CreatedOn { get; set; }
     public DateTime? ModifiedOn { get; set; }
 
