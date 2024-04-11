f9d1eeb50155fe7e793a690353c4bd38c1dfa68b
JuneMaths <a.juliaadamska@gmail.com>
2024-04-11 09:30:21 +0200

    add create and delete

--- Server/src/Currencies.DataAccess/Services/RoleService.cs
+++ Server/src/Currencies.DataAccess/Services/RoleService.cs
@@ -1,31 +1,67 @@
-﻿using Currencies.Contracts.Helpers;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Currencies.Contracts.Helpers;
 using Currencies.Contracts.Interfaces;
 using Currencies.Contracts.ModelDtos.Role;
 using Currencies.Models;
 using Currencies.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Currencies.DataAccess.Services;
 
 public class RoleService : IRoleService
 {
     private readonly TableContext _dbContext;
+    private readonly IMapper _mapper;
 
-    public RoleService(TableContext dbContext)
+    public RoleService(TableContext dbContext, IMapper mapper)
     {
         _dbContext = dbContext;
+        _mapper = mapper;
     }
 
     public async Task<RoleDto?> CreateAsync(BaseRoleDto dto, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (dto == null)
+        {
+            return null;
+        }
+
+        var role = new Role()
+        {
+            Name = dto.Name        
+        };
+
+        _dbContext.Roles.Add(role);
+
+        if (await _dbContext.SaveChangesAsync() > 0)
+        {
+            return _mapper.Map<RoleDto>(role);
+        }
+
+        throw new DbUpdateException($"Could not save changes to database at: {nameof(CreateAsync)}");
+
     }
 
     public async Task<bool> DeleteAsync(int id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var role = await GetByIdAsync(id, cancellationToken);
+        if (role == null || !role.IsActive)
+        {
+            return false;
+        }
+
+        role.IsActive = false;
+
+        if ((await _dbContext.SaveChangesAsync()) > 0)
+        {
+            return true;
+        }
+
+        throw new DbUpdateException($"Could not save changes to database at: {nameof(DeleteAsync)}");
     }
 
-    public async Task<PageResult<RoleDto>> GetAllRolesAsync(FilterRoleDto filter, CancellationToken cancellationToken)
+    public async Task<PageResult<RoleDto>?> GetAllRolesAsync(FilterRoleDto filter, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
