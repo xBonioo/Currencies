8ac4d2a78c3f56af9a896f01014e2c3e19b54bd5
DawidStepniewski9 <dawlew999@gmail.com>
2024-05-01 11:21:08 +0200

    Fixed FilterExchangeRateDto name

--- Server/src/Currencies.Contracts/Interfaces/IExchangeRateService.cs
+++ Server/src/Currencies.Contracts/Interfaces/IExchangeRateService.cs
@@ -7,7 +7,7 @@ namespace Currencies.Contracts.Interfaces;
 
 public interface IExchangeRateService : IEntityService<ExchangeRate>
 {
-    Task<PageResult<ExchangeRateDto>> GetAllExchangeRateAsync(FilterDto filter,CancellationToken cancellationToken);
+    Task<PageResult<ExchangeRateDto>> GetAllExchangeRateAsync(FilterExchangeRateDto filter,CancellationToken cancellationToken);
     Task<ExchangeRateDto?> CreateAsync(BaseExchangeRateDto dto, CancellationToken cancellationToken);
     Task<ExchangeRateDto?> UpdateAsync(int id, BaseExchangeRateDto dto, CancellationToken cancellationToken);
     Task<bool> DeleteAsync(int id, CancellationToken cancellationToken);
